69e21f89140ca35ec98f4cb266a4f8a0a94b3813
5hapeshifter <suleimandiverio@gmail.com>
2022-05-04 16:03:17 -0300

    Anotação adicionada

--- backend/src/test/java/com/devsuperior/dscatalog/DscatalogApplicationTests.java
+++ backend/src/test/java/com/devsuperior/dscatalog/DscatalogApplicationTests.java
@@ -3,9 +3,13 @@ package com.devsuperior.dscatalog;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest //Carrega o contexto da aplicação, utilizada mais em testes de integração
 class DscatalogApplicationTests {
 
+    /** Contexto na aplicação Spring corresponde a todos os componentes do Spring, a toda infraestrutura do Spring
+     * isso impactaria muito nos testes, pois carregaria toda a estrutura para cada teste que fosse rodado e não
+     * conseguiríamos testar individualmente uma classe, por exemplo
+     */
     @Test
     void contextLoads() {
     }
