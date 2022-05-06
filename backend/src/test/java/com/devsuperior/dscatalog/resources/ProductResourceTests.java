1fbb17a02d6e9c49f6988e4c72da9eec4829b246
5hapshifter <suleimandiverio@gmail.com>
2022-05-05 22:30:45 -0300

    Testes dos metodos da camada web findAll e update concluidos

--- backend/src/test/java/com/devsuperior/dscatalog/resources/ProductResourceTests.java
+++ backend/src/test/java/com/devsuperior/dscatalog/resources/ProductResourceTests.java
@@ -2,7 +2,9 @@ package com.devsuperior.dscatalog.resources;
 
 import com.devsuperior.dscatalog.dto.ProductDTO;
 import com.devsuperior.dscatalog.services.ProductService;
+import com.devsuperior.dscatalog.services.exceptions.ResourceNotFoundException;
 import com.devsuperior.dscatalog.tests.Factory;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.ArgumentMatchers;
@@ -11,11 +13,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.data.domain.PageImpl;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import java.util.List;
+
+import static org.mockito.ArgumentMatchers.any;
 
 @WebMvcTest(ProductResource.class)
 public class ProductResourceTests {
@@ -26,25 +32,94 @@ public class ProductResourceTests {
     @MockBean // Precisamos utilizar o @MockBean pq estamos na camada web
     private ProductService service;
 
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    private long existingId;
+    private long nonExistingId;
     private ProductDTO productDTO;
     private PageImpl<ProductDTO> page; // Objeto concreto ao inves do Page que é uma interface
 
     @BeforeEach
     void setUp() throws Exception{
+        existingId = 1L;
+        nonExistingId = 2L;
+
         productDTO = Factory.createProductDto();
         page = new PageImpl<>(List.of(productDTO));
 
         // Comportamentos
-        Mockito.when(service.findAllPaged(ArgumentMatchers.any())).thenReturn(page);
+        Mockito.when(service.findAllPaged(any())).thenReturn(page);
+
+        Mockito.when(service.findById(existingId)).thenReturn(productDTO);
+        Mockito.when(service.findById(nonExistingId)).thenThrow(ResourceNotFoundException.class);
+
+        Mockito.when(service.update(ArgumentMatchers.eq(existingId), any())).thenReturn(productDTO);
+        Mockito.when(service.update(ArgumentMatchers.eq(nonExistingId), any())).thenThrow(ResourceNotFoundException.class);
 
     }
 
     @Test
-    public void findAllShouldReturnPage() throws Exception {
-        mockMvc.perform(MockMvcRequestBuilders.get("/products"))
-                .andExpect(MockMvcResultMatchers.status().isOk());
+    public void updateShouldReturnProductDTOWhenIdExist() throws Exception {
+        // Para poder fazer o teste estamos convertando o tipo do objeto da resposta de Json para Java
+        String jsonBody = objectMapper.writeValueAsString(productDTO);
+        ResultActions result =
+                mockMvc.perform(MockMvcRequestBuilders.put("/products/{id}", existingId)
+                        .content(jsonBody)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON));
+        // O '$' acessa o atributo no Json, nesse caso estamos verificando se o id, name, description existem
+        result.andExpect(MockMvcResultMatchers.status().isOk());
+        result.andExpect(MockMvcResultMatchers.jsonPath("$.id").exists());
+        result.andExpect(MockMvcResultMatchers.jsonPath("$.name").exists());
+        result.andExpect(MockMvcResultMatchers.jsonPath("$.description").exists());
     }
 
+    @Test
+    public void updateShouldReturnNotFoundWhenIdDoesNotExist() throws Exception {
+        // Para poder fazer o teste estamos convertando o tipo do objeto da resposta de Json para Java
+        String jsonBody = objectMapper.writeValueAsString(productDTO);
+        ResultActions result =
+                mockMvc.perform(MockMvcRequestBuilders.put("/products/{id}", nonExistingId)
+                        .content(jsonBody)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON));
+        // O '$' acessa o atributo no Json, nesse caso estamos verificando se o id, name, description existem
+        result.andExpect(MockMvcResultMatchers.status().isNotFound());
+    }
 
+    @Test
+    public void findAllShouldReturnPage() throws Exception {
+        ResultActions result =
+                mockMvc.perform(MockMvcRequestBuilders.get("/products")
+                        .accept(MediaType.APPLICATION_JSON));
+        //
+        result.andExpect(MockMvcResultMatchers.status().isOk());
+    }
+
+    @Test
+    public void findByIdShouldReturnProductWhenIdExists() throws Exception {
+        ResultActions result =
+                mockMvc.perform(MockMvcRequestBuilders.get("/products/{id}", existingId)
+                        .accept(MediaType.APPLICATION_JSON));
+
+        result.andExpect(MockMvcResultMatchers.status().isOk());
+
+        // O '$' acessa o atributo no Json, nesse caso estamos verificando se o id, name, description existem
+        result.andExpect(MockMvcResultMatchers.jsonPath("$.id").exists());
+        result.andExpect(MockMvcResultMatchers.jsonPath("$.name").exists());
+        result.andExpect(MockMvcResultMatchers.jsonPath("$.description").exists());
+    }
+
+    /*
+     O retorno deve ser o status do método da camada web, not found nesse caso
+     */
+    @Test
+    public void findByIdShouldReturnNotFoundWhenIdDoesNotExists() throws Exception{
+        ResultActions result =
+                mockMvc.perform(MockMvcRequestBuilders.get("/products/{id}", nonExistingId)
+                        .accept(MediaType.APPLICATION_JSON));
+        result.andExpect(MockMvcResultMatchers.status().isNotFound());
+    }
 
 }
