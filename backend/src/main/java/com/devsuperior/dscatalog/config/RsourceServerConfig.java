4bf797a47bc98fce609f40d03cf3418f615088f4
5hapshifter <suleimandiverio@gmail.com>
2022-05-25 21:47:34 -0300

    H2 security configuration

--- backend/src/main/java/com/devsuperior/dscatalog/config/RsourceServerConfig.java
+++ backend/src/main/java/com/devsuperior/dscatalog/config/RsourceServerConfig.java
@@ -2,6 +2,7 @@ package com.devsuperior.dscatalog.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
@@ -9,15 +10,21 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.R
 import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
 import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 
+import java.util.Arrays;
+
 @Configuration
 @EnableResourceServer // Classe responsavel por receber o token e verificar se o acesso da requisicao esta ok
 public class RsourceServerConfig extends ResourceServerConfigurerAdapter {
+
+    // a partir desse objeto conseguimos acessar diversas variaveis da aplicacao
+    @Autowired
+    private Environment env;
 
     @Autowired
     private JwtTokenStore tokenStore;
 
     // rota que sera publica para acessar
-    private static final String[] PUBLIC = {"/oauth/token"};
+    private static final String[] PUBLIC = {"/oauth/token", "/h2-console/**"};
 
     // rota com nivel de acesso
     private static final String[] OPERATOR_OR_ADMIN = {"/products/**", "/categories/**"};
@@ -34,6 +41,11 @@ public class RsourceServerConfig extends ResourceServerConfigurerAdapter {
     // configuracao das rotas
     @Override
     public void configure(HttpSecurity http) throws Exception {
+
+        // configuracao para liberar o console do h2
+        if (Arrays.asList(env.getActiveProfiles()).contains("test")) {
+            http.headers().frameOptions().disable();
+        }
 
         http.authorizeRequests()
                 .antMatchers(PUBLIC).permitAll()
