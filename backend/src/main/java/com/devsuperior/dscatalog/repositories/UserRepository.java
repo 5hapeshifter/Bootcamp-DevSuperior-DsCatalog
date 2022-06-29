af5300c9a8e66e58870e82c8b2bdef6cc2bf19da
5hapeshifter <suleimandiverio@gmail.com>
2022-06-29 16:36:16 -0300

    QueryMethods User

--- backend/src/main/java/com/devsuperior/dscatalog/repositories/UserRepository.java
+++ backend/src/main/java/com/devsuperior/dscatalog/repositories/UserRepository.java
@@ -4,9 +4,15 @@ import com.devsuperior.dscatalog.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
+    //QueryMethods - pega o padrão de escrita e converte na execucao, entao temos que concatenar o findBy + nomeDoAtributo
+    //Mais indicado para consultas simples
     User findByEmail(String email);
+    //QueryMethod para buscar uma lista de usuarios pelo primeiro nome
+    List<User> findByFirstName(String firstName);
 
 }
